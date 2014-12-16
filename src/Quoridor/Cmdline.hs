8a860fb33cde974faad8a6554a9d7ba3dd2ee70f
Tal Walter <talw10@gmail.com>
2014-12-16 09:36:59 +0200

    Refactored playLocal in an alternative style, hopefully I can get some opinions as to whether or not this is an improvement over the older style

--- src/Quoridor/Cmdline.hs
+++ src/Quoridor/Cmdline.hs
@@ -5,7 +5,7 @@ module Quoridor.Cmdline
 import Control.Applicative  ((<$>))
 import Control.Monad        (when)
 import Control.Monad.Reader (ask)
-import Control.Monad.State  (get, liftIO)
+import Control.Monad.State  (MonadIO, get, liftIO)
 import Data.List            (sort)
 import System.Environment   (getArgs)
 import System.Exit          (exitSuccess)
@@ -37,26 +37,47 @@ cmdlineMain = do
   exitSuccess
 
 playLocal :: Game IO ()
-playLocal = do
-  gc <- ask
-  let go showBoard msg = do
-        gs <- get
-        vm <- sort <$> getCurrentValidMoves
-        when showBoard $ liftIO $ renderBoard gs gc vm
-        liftIO $ putStrLn msg
-        case winner gs of
-          Just c  -> liftIO $ putStrLn $ msgGameEnd c
-          Nothing -> do
-            strTurn <- liftIO getLine
-            let eTurn = parseTurn strTurn
-            case eTurn of
-              Left invalidParseMsg -> go False invalidParseMsg
-              Right turn -> do
-                wasValid <- makeTurn turn
-                go True $ if wasValid
-                  then msgValidTurn (color $ currP gs) turn
-                  else msgInvalidTurn
-  go True msgInitialTurn
+playLocal = go True msgInitialTurn
+  where go showBoard msg = do
+          gs <- get
+          let parseFailAct = go False
+              parseSuccAct turn = do wasValid <- makeTurn turn
+                                     go True $ if wasValid
+                                       then msgValidTurn (color $ currP gs) turn
+                                       else msgInvalidTurn
 
+          when showBoard renderCurrentBoard
+          liftIO $ putStrLn msg
+          handleWinOrTurn gs
+            wonAction $
+            handleParse (liftIO getLine) parseFailAct parseSuccAct
+
+renderCurrentBoard :: (Functor m, MonadIO m) => Game m ()
+renderCurrentBoard = do
+  gc <- ask
+  gs <- get
+  vm <- sort <$> getCurrentValidMoves
+  liftIO $ renderBoard gs gc vm
+
+-- Function that could be common to playLocal,
+-- playClient(handleWinOrTurn, wonAction, renderBoard) and playServer(handleParse)
+-- if I change playClient and playServer to be in the same vein as playLocal
 renderBoard :: GameState -> GameConfig -> [Cell] -> IO ()
 renderBoard gs gc vms = putColoredStr $ runRenderColor gs gc vms
+
+handleParse :: MonadIO m =>
+  m String -> (String -> m ()) -> (Turn -> m ()) -> m ()
+handleParse strAct failAct succAct = do
+  strTurn <- strAct
+  case parseTurn strTurn of
+    Left msg   -> failAct msg
+    Right turn -> succAct turn
+
+handleWinOrTurn :: MonadIO m => GameState -> (Color -> m ()) -> m () -> m ()
+handleWinOrTurn gs wonAct contAct =
+  case winner gs of
+    Just c  -> wonAct c
+    Nothing -> contAct
+
+wonAction :: MonadIO m => Color -> m ()
+wonAction = liftIO . putStrLn . msgGameEnd
