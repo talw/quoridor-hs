dfddcb17db63bf86f471136ef59ccebfd02fc2d0
Tal Walter <talw10@gmail.com>
2014-12-21 18:07:57 +0200

    Added web resources, based on Jasper's blog post, with modifications where neccessary

---
+++ resources/console.js
@@ -0,0 +1,52 @@
+function appendOutput(cls, text) {
+    $('#console-output').append('<pre class="' + cls + '">' + text + '</pre>');
+    $('#line').focus();
+    $('#line')[0].scrollIntoView({block: "end", behavior: "instant"});
+}
+
+function appendChar(c) {
+
+}
+
+$(document).ready(function () {
+    var ws;
+
+    $('#port').focus();
+
+    $('#login').submit(function () {
+        $('#login').css('display', 'none');
+        $('#console').css('display', 'block');
+
+        var port = $('#port').val();
+        var hostName = window.location.hostname;
+        var uri = "ws://"+hostName+":"+port+"/play"
+        ws = new WebSocket(uri);
+        appendOutput('stderr', 'Opening WebSockets connection...\n');
+
+        ws.onerror = function(event) {
+            appendOutput('stderr', 'WebSockets error: ' + event.data + '\n');
+        };
+
+        ws.onopen = function() {
+            appendOutput('stderr', 'WebSockets connection successful!\n');
+        };
+
+        ws.onclose = function() {
+            appendOutput('stderr', 'WebSockets connection closed.\n');
+        };
+
+        ws.onmessage = function(event) {
+            appendOutput('stdout', event.data);
+        };
+
+        return false;
+    });
+
+    $('#console-input').submit(function () {
+        var line = $('#line').val();
+        ws.send(line + '\n');
+        appendOutput('stdin', line + '\n');
+        $('#line').val('');
+        return false;
+    });
+});
